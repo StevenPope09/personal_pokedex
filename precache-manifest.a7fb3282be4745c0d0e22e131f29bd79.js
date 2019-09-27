7d63681438fa858ec62b3ed47c73f90e975dec07
Steven Pope <sjpope@student.fullsail.edu>
2019-09-27 13:34:29 -0500

    Updates

---
+++ precache-manifest.a7fb3282be4745c0d0e22e131f29bd79.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4bb207f2e3e60aaa4d9ff2a23eb9aa",
+    "url": "/personal_pokedex/index.html"
+  },
+  {
+    "revision": "c688fd6de31ddcdf39ae",
+    "url": "/personal_pokedex/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "58b85a177b30392cd038",
+    "url": "/personal_pokedex/static/css/main.e42c3ec2.chunk.css"
+  },
+  {
+    "revision": "c688fd6de31ddcdf39ae",
+    "url": "/personal_pokedex/static/js/2.dc211160.chunk.js"
+  },
+  {
+    "revision": "58b85a177b30392cd038",
+    "url": "/personal_pokedex/static/js/main.b3040e01.chunk.js"
+  },
+  {
+    "revision": "f84785852c3b44bf99c1",
+    "url": "/personal_pokedex/static/js/runtime~main.09ebdb87.js"
+  },
+  {
+    "revision": "c9f7539c2d58d9ff743973ab310b9270",
+    "url": "/personal_pokedex/static/media/adPic.c9f7539c.jpeg"
+  },
+  {
+    "revision": "2e4c603b2596fe1ad54fbaf6af6acb6d",
+    "url": "/personal_pokedex/static/media/customControllers.2e4c603b.jpeg"
+  },
+  {
+    "revision": "5e0a1e29ba7e90d6b5743b7696924096",
+    "url": "/personal_pokedex/static/media/heroic.5e0a1e29.jpeg"
+  },
+  {
+    "revision": "920376e22d9cb07ef9c098b5b5a30ee6",
+    "url": "/personal_pokedex/static/media/legendary.920376e2.jpeg"
+  },
+  {
+    "revision": "c761ab7f5779c6dff58ba9d2b5d27dd9",
+    "url": "/personal_pokedex/static/media/logo.c761ab7f.png"
+  },
+  {
+    "revision": "e44f54d8396d1a237dc782d99f106d29",
+    "url": "/personal_pokedex/static/media/newsPoke.e44f54d8.jpeg"
+  },
+  {
+    "revision": "3768d1b90cd7ee4398bb97df3f5e52f2",
+    "url": "/personal_pokedex/static/media/pokeSwitch.3768d1b9.jpeg"
+  },
+  {
+    "revision": "48b539e9ef6bb910a5fd3b489a73c153",
+    "url": "/personal_pokedex/static/media/pokemonMasters.48b539e9.jpeg"
+  },
+  {
+    "revision": "c99ead19b310f93c8d860d7c2291d69d",
+    "url": "/personal_pokedex/static/media/pokemonSwitch.c99ead19.jpeg"
+  }
+]);
